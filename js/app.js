3d9cf74b9d82986524c13ee697632b4ca69b66a2
Rufus Pollock <rufus.pollock@okfn.org>
2012-09-29 20:18:57 +0100

    [all][m]: the app is working!.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
+// time in seconds for the test
+var COUNTDOWN = 60;
+
 jQuery(document).ready(function($) {
   var group = window.location.search.split('=')[1];
+  if (group === '1') {
+    // default
+  } else if (group === '2') {
+    $('.reward span').text('50p'); 
+  } else {
+    $('.reward').hide();
+  }
   switchView('instructions'); 
   $('.start').click(function(e) {
     switchView('words');
-    countdown(60, function() {
-      alert('done');
+    countdown(COUNTDOWN, function() {
+      switchView('test');
     });
+  });
+
+  var words = $('.words').find('pre').text().split('\n');
+  $('.done').click(function(e) {
+    var answers = $('.test textarea').val().replace(', ', ' ').replace(',', ' ').split(/[\n ]/);
+    var numCorrect = 0;
+    for(var idx=0; idx<answers.length; idx++) {
+      if ($.inArray(answers[idx].toLowerCase(), words) != -1) {
+        numCorrect += 1;
+      }
+    }
+    $('.num-correct').text(numCorrect);
+    switchView('results');
   });
 });
 
